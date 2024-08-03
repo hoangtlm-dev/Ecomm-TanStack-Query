0a3bcba4a7dd474d3aab2a331140bc05332f862a
tranleminhhoang2410 <hoangtlm2410@gmail.com>
2024-08-04 01:13:57 +0700

    feat: implement ui & storybook for navbar

--- src/components/Header/index.ts
+++ src/components/Header/index.ts
@@ -1,1 +1,2 @@
 export { default as BrandingLogo } from './BrandingLogo'
+export { default as Navbar } from './Navbar'
--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -10,3 +10,26 @@ export const ROUTES = {
   BELT: '/belt',
   CONTACT: '/contact'
 }
+
+export const NAV_MENUS = [
+  {
+    pageName: 'Home',
+    route: ROUTES.ROOT
+  },
+  {
+    pageName: 'Bag',
+    route: ROUTES.BAGS
+  },
+  {
+    pageName: 'Sneakers',
+    route: ROUTES.SNEAKERS
+  },
+  {
+    pageName: 'Belt',
+    route: ROUTES.BELT
+  },
+  {
+    pageName: 'Contact',
+    route: ROUTES.CONTACT
+  }
+]
