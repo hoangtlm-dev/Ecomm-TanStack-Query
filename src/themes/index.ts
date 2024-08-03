c090cc2451ea18d275ec2ba3cd06a209e72de4ee
tranleminhhoang2410 <hoangtlm2410@gmail.com>
2024-08-04 00:46:51 +0700

    feat: custom component for chakra-ui

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -3,10 +3,16 @@ import { extendTheme } from '@chakra-ui/react'
 // Base customize
 import { breakpoints, colors, fonts, fontSizes, metrics } from './base'
 
+// Component customize
+import { ButtonCustom, ContainerCustom, LinkCustom, TextCustom } from './components'
+
 export const theme = extendTheme({
   colors,
   components: {
-    // TODO: custom components
+    Button: ButtonCustom,
+    Text: TextCustom,
+    Link: LinkCustom,
+    Container: ContainerCustom
   },
   sizes: {
     container: metrics.containerSize
