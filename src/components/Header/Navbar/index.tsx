f15949c22139b8e3716b452a1844be8fbbad5ed5
tranleminhhoang2410 <hoangtlm2410@gmail.com>
2024-08-07 23:45:39 +0700

    fix: update navbar ui

--- src/components/Header/Navbar/index.tsx
+++ src/components/Header/Navbar/index.tsx
@@ -1,4 +1,5 @@
-import { HStack, Link, IconButton, Container, Menu, MenuButton, MenuList } from '@chakra-ui/react'
+import { useState } from 'react'
+import { HStack, Link, IconButton, Container, Stack } from '@chakra-ui/react'
 import { NavLink } from 'react-router-dom'
 
 // Constants
@@ -6,6 +7,9 @@ import { NAV_MENUS } from '@app/constants'
 
 // Components
 import { BrandingLogo, HamburgerIcon } from '@app/components'
+import { AnimatePresence, motion } from 'framer-motion'
+
+const MotionStack = motion(Stack)
 
 interface INavMenuLinksProps {
   onClose?: () => void
@@ -33,27 +37,54 @@ const NavMenuLinks = ({ onClose }: INavMenuLinksProps) => {
 }
 
 const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen((prev) => !prev)
+  }
+
+  const closeMobileMenu = () => {
+    setIsMobileMenuOpen(false)
+  }
+
   return (
     <Container>
-      <HStack justifyContent="space-between" pt={4}>
+      <HStack justifyContent="space-between" p={4} position="relative" zIndex={2}>
         <BrandingLogo />
         <HStack as="nav" spacing={10} display={{ base: 'none', lg: 'flex' }}>
           <NavMenuLinks />
         </HStack>
-
-        <Menu>
-          <MenuButton
-            as={IconButton}
-            aria-label="Open Menu"
-            icon={<HamburgerIcon boxSize={5} color="black" />}
-            variant="unstyled"
-            textAlign="right"
-            display={{ base: 'block', lg: 'none' }}
-          />
-          <MenuList as="nav" display={{ base: 'flex', lg: 'none' }} flexDirection="column" w="full">
-            <NavMenuLinks />
-          </MenuList>
-        </Menu>
+        <IconButton
+          aria-label="Open Menu"
+          display={{ base: 'block', lg: 'none' }}
+          variant="unstyled"
+          textAlign="right"
+          icon={<HamburgerIcon boxSize={5} color="black" />}
+          onClick={toggleMobileMenu}
+        />
+        <AnimatePresence>
+          {isMobileMenuOpen && (
+            <MotionStack
+              p={4}
+              as="nav"
+              spacing={4}
+              display={{ base: 'flex', lg: 'none' }}
+              position="absolute"
+              top={16}
+              left={0}
+              right={0}
+              backgroundColor="backgroundWhite"
+              zIndex={1}
+              boxShadow="lg"
+              initial={{ y: -64 }}
+              animate={{ y: 0 }}
+              exit={{ y: -64 }}
+              transition={{ duration: 0.3 }}
+            >
+              <NavMenuLinks onClose={closeMobileMenu} />
+            </MotionStack>
+          )}
+        </AnimatePresence>
       </HStack>
     </Container>
   )
