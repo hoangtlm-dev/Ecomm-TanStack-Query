36b7f77cc588a34edb2e0edf047a40324c2b5e66
tranleminhhoang2410 <hoangtlm2410@gmail.com>
2024-09-01 00:30:09 +0700

    chore: update type for query params

--- src/types/query-params.ts
+++ src/types/query-params.ts
@@ -1,4 +1,4 @@
-export type QueryParams<T extends Record<string, string | number | boolean>> = {
+export type QueryParams<T extends Record<string, string | string[] | number | boolean>> = {
   page?: number
   limit?: number
   q?: string
@@ -14,7 +14,7 @@ type AddSuffix<T, Suffix extends string> = {
   [K in keyof T as `${Extract<K, string>}${Suffix}`]?: T[K] extends string[] ? string : T[K]
 }
 
-export type ExtendedQueryParams<T extends Record<string, string | number | boolean>> = QueryParams<T> &
+export type ExtendedQueryParams<T extends Record<string, string | string[] | number | boolean>> = QueryParams<T> &
   AddSuffix<T, '_ne'> &
   AddSuffix<T, '_gte'> &
   AddSuffix<T, '_lte'> &
