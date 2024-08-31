fc3ef449cfb64101139a8f595c9341abde6c433a
tranleminhhoang2410 <hoangtlm2410@gmail.com>
2024-09-01 00:27:22 +0700

    chore: update colors for mock product

--- src/mocks/product.ts
+++ src/mocks/product.ts
@@ -9,6 +9,7 @@ export const MOCK_PRODUCT = {
   name: 'Nike Airmax 270 React',
   description:
     'Nunc facilisis sagittis ullamcorper. Proin lectus ipsum, gravida et mattis vulputate, tristique ut lectus. Sed et lectus lorem nunc leifend laorevtr istique et congue. Vivamus adipiscin vulputate g nisl ut dolor. Nunc facilisis sagittis ullamcorper. Proin lectus ipsum, gravida et mattis vulputate, tristique ut lectus. Sed et lectus lorem nunc leifend laorevtr istique et congue. Vivamus adipiscin vulputate g nisl ut dolor. Nunc facilisis sagittis ullamcorper. Proin lectus ipsum, gravida et mattis vulputate, tristique ut lectus. Sed et lectus lorem nunc leifend laorevtr istique et congue. Vivamus adipiscin vulputate g nisl ut dolor. Nunc facilisis sagittis ullamcorper. Proin lectus ipsum, gravida et mattis vulputate, tristique ut lectus. Sed et lectus lorem nunc leifend laorevtr istique et congue. Vivamus adipiscin vulputate g nisl ut dolor .',
+  colors: ['blue', 'white', 'pink'],
   price: 539.99,
   currencyUnit: '$',
   quantity: 100,
